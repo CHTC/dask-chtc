42e7980d5761f4a975868ee3632f962146bd6fda
Josh Karpel <josh.karpel@gmail.com>
2020-06-19 09:19:08 -0500

    bare test

---
+++ test-bare-distributed.py
@@ -0,0 +1,18 @@
+from dask.distributed import Client, LocalCluster
+import dask.array as da
+
+if __name__ == '__main__':
+    with LocalCluster() as cluster, Client(cluster) as client:
+        print(cluster)
+
+        print(client)
+
+        x = da.random.random((10000, 10000), chunks=(1000, 1000))
+
+        y = (x ** 2).sum()
+
+        print(y)
+
+        z = y.compute()
+
+        print(z)
