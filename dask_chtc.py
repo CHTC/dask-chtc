a91ebcb5ac4603f37ffee3e62d7f445de2d205fa
Josh Karpel <josh.karpel@gmail.com>
2020-06-23 14:29:50 -0500

    progress toward gpu workers

--- dask_chtc.py
+++ dask_chtc.py
@@ -4,11 +4,33 @@ import sys
 from dask_jobqueue import HTCondorCluster
 
 
-def CHTCCluster(worker_image=None):
+def CHTCCluster(worker_image=None, gpus=False):
     shutil.rmtree("logs", ignore_errors=True)
 
     if worker_image is None:
         worker_image = sys.argv[1]
+
+    job_extra = {
+        "universe": "docker",
+        "docker_image": worker_image,
+        "container_service_names": "dask",
+        "dask_container_port": "8787",
+        "requirements": "(Target.HasCHTCStaging)",
+        "keep_claim_idle": "600",
+        "My.IsDaskWorker": "true",
+        "My.wantFlocking": "true",
+        "JobBatchName": "dask-worker",
+    }
+
+    if gpus:
+        job_extra.update(
+            {
+                "request_gpus": "1",
+                "My.WantGPULab": "true",
+                "My.GPUJobLength": '"short"',
+                "JobBatchName": "dask-worker-gpu",
+            }
+        )
 
     return HTCondorCluster(
         cores=1,
@@ -17,12 +39,5 @@ def CHTCCluster(worker_image=None):
         log_directory="logs",
         python="python3",
         scheduler_options={"dashboard_address": "3400", "port": 3500},
-        job_extra={
-            "universe": "docker",
-            "docker_image": worker_image,
-            "container_service_names": "dask",
-            "dask_container_port": "8787",
-            "requirements": "(Target.HasCHTCStaging)",
-            "keep_claim_idle": "3600",
-        },
+        job_extra=job_extra,
     )
