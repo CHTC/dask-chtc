75c09edfcef13609a41d4675e80e04f3bbbf80ed
Josh Karpel <josh.karpel@gmail.com>
2020-07-07 12:48:36 -0500

    add defaults for cores, memory, and disk; other cleanup

--- dask_chtc/cluster.py
+++ dask_chtc/cluster.py
@@ -1,16 +1,16 @@
 import collections
 import datetime
 import logging
+import math
 import os
 from pathlib import Path
-from typing import Any, Dict, Iterable, List, Mapping, Optional
+from typing import Any, Dict, Iterable, Mapping, Optional
 
 from dask_jobqueue import HTCondorCluster
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-TEN_MINUTES = datetime.timedelta(minutes=10).total_seconds()
 
 PACKAGE_DIR = Path(__file__).parent
 ENTRYPOINT_SCRIPT_PATH = (PACKAGE_DIR / "entrypoint.sh").absolute()
@@ -20,6 +20,9 @@ class CHTCCluster(HTCondorCluster):
     def __init__(
         self,
         *,
+        cores: int = 1,
+        memory: str = "1 GB",
+        disk: str = "10 GB",
         worker_image: Optional[str] = None,
         input_files: Optional[Iterable[os.PathLike]] = None,
         gpu_lab: bool = False,
@@ -31,7 +34,7 @@ class CHTCCluster(HTCondorCluster):
             kwargs, worker_image=worker_image, input_files=input_files, gpu_lab=gpu_lab, gpus=gpus,
         )
 
-        super().__init__(python=python, **kwargs)
+        super().__init__(cores=cores, memory=memory, disk=disk, python=python, **kwargs)
 
     @staticmethod
     def _modify_kwargs(
@@ -44,7 +47,7 @@ class CHTCCluster(HTCondorCluster):
     ) -> Dict[str, Any]:
         modified = kwargs.copy()
 
-        # These get forward to the Dask scheduler
+        # These get forwarded to the Dask scheduler
         modified["scheduler_options"] = merge(
             # Capture anything the user passed in.
             {"port": 3500, "dashboard_address": str(3400)},
@@ -80,7 +83,7 @@ class CHTCCluster(HTCondorCluster):
             # Capture anything the user passed in.
             kwargs.get("job_extra"),
             # Overrideable utility/convenience attributes
-            {"JobBatchName": "dask-worker", "keep_claim_idle": str(TEN_MINUTES)},
+            {"JobBatchName": "dask-worker", "keep_claim_idle": seconds(minutes=10)},
         )
 
         # These get tacked on to the command that starts the worker as arguments
@@ -96,6 +99,26 @@ class CHTCCluster(HTCondorCluster):
 def merge(*mappings: Optional[Mapping[Any, Any]]) -> Mapping[Any, Any]:
     """
     Merge the given mappings into a single mapping.
-    Mappings given earlier in the list have precedence over those given later.
+    Mappings given earlier in the args have precedence over those given later.
     """
     return dict(collections.ChainMap(*filter(None, mappings)))
+
+
+def seconds(**kwargs: int) -> int:
+    """
+    Produce an integer number of seconds from keyword arguments,
+    like ``minutes = 5``.
+
+    Parameters
+    ----------
+    kwargs
+        Any keyword argument that could be passed to :class:`datetime.timedelta`.
+
+    Returns
+    -------
+    seconds : int
+        The **integer** number of seconds,
+        rounded up,
+        represented by the ``kwargs``.
+    """
+    return math.ceil(datetime.timedelta(**kwargs).total_seconds())
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,5 @@
 import dask_chtc
 
 
-def test_has_dunder_version():
+def test_module_has_dunder_version():
     assert hasattr(dask_chtc, "__version__")
